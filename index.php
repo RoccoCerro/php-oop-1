bd70debe539508db7e5c5d3b07992e56bd0c73b4
RoccoCerro <rcerro02@gmail.com>
2024-05-11 16:51:14 +0200

    Add: style header

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require_once __DIR__ . "/models/Movie.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>OOP</title>
+  <link rel="stylesheet" href="./style/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -22,8 +23,10 @@ require_once __DIR__ . "/models/Movie.php";
   <div id="app">
 
     <!-- Header -->
-    <header>
-        
+    <header class="page-header">
+      <?php
+      require_once __DIR__."/partials/template/header.php"
+      ?>
     </header>
 
     <!-- Main -->
---
+++ partials/template/header.php
@@ -0,0 +1,6 @@
+<div class="container">
+  <div class="row justify-content-between align-items-center">
+    <div class="col-3"><img src="img/loremIpsum3.png" alt=""></div>
+    <div class="col-3">Home - User</div>
+  </div>
+</div>
